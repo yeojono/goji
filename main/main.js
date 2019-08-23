b8c95a20e494507740955b4d078f178141b33a4b
Jonathan Yeo <jyeo@atlassian.com>
2019-08-23 12:39:35 +1000

    what the heck is going on

--- main/main.js
+++ main/main.js
@@ -24,27 +24,12 @@ function createWindow () {
   win.loadURL(indexFilename);
   win.webContents.openDevTools();
 
-  if (process.env.NODE_ENV === 'development') {
-    log = require('electron-log');
-    autoUpdater.updateConfigPath = path.join(__dirname, '../dev-app-update.yml');
-    autoUpdater.allowPrerelease = true;
-    log.info('Current version: ', autoUpdater.currentVersion);
-    autoUpdater.currentVersion.major = 0;
-    autoUpdater.currentVersion.minor = 9;
-    log.info('Current version: ', autoUpdater.currentVersion);
-  }
-
   function sendMessageToWindow(text) {
-    if (process.env.NODE_ENV === 'development') {
-      log.info('sendMessageToWindow:', text);
-    }
     win.webContents.send('message', text);
   }
 
   autoUpdater.on('checking-for-update', (event) => {
     sendMessageToWindow('Checking for updates');
-    log.info('=====================')
-    log.info(event)
   });
 
   autoUpdater.on('update-available', (event) => {
